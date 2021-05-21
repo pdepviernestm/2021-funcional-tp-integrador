f67c336042d17847798c76e929c3da454f75e552
juanFdS <juan.fernandes@10pines.com>
2021-05-21 12:04:32 -0300

    Punto 1 resuelto

--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,68 @@
 module Library where
 import PdePreludat
 
+data Persona = Persona {
+    felicidonios :: Number,
+    sueños :: Number,
+    edad :: Number
+}
+
+data Felicidad = Alta | Media | Baja deriving (Show, Eq)
+
+nivelDeFelicidad :: Persona -> Felicidad
+nivelDeFelicidad persona    
+    |felicidonios persona <= 50 = Baja
+    |felicidonios persona > 50 && felicidonios persona <= 100 = Media
+    |felicidonios persona > 100 = Alta
+
+muyFeliz :: Persona -> Bool
+muyFeliz persona = nivelDeFelicidad persona == Alta
+
+moderadamenteFeliz :: Persona -> Bool
+moderadamenteFeliz persona = nivelDeFelicidad persona == Media
+
+pocoFeliz :: Persona -> Bool
+pocoFeliz persona = nivelDeFelicidad persona == Baja
+
+coeficienteDeSatisfaccion :: Persona -> Number
+coeficienteDeSatisfaccion persona
+    | pocoFeliz persona = felicidonios persona `div` 2
+    | moderadamenteFeliz persona = felicidonios persona * sueños persona
+    | muyFeliz persona = felicidonios persona * edad persona
+
+gradoDeAmbicion :: Persona -> Number
+gradoDeAmbicion persona
+    | moderadamenteFeliz persona = edad persona * sueños persona
+    | muyFeliz persona = felicidonios persona *sueños persona
+    | pocoFeliz persona = 2 * sueños persona
+
+--Alternativas:
+-- coeficienteDeSatisfaccion :: Persona -> Number
+-- coeficienteDeSatisfaccion persona
+--     |nivelDeFelicidad persona == Baja = felicidonios persona `div` 2
+--     |nivelDeFelicidad persona == Media = felicidonios persona * sueños persona
+--     |nivelDeFelicidad persona == Alta = felicidonios persona * edad persona
+
+-- coeficienteDeSatisfaccion' :: Persona -> Number
+-- coeficienteDeSatisfaccion persona =
+--     coeficienteDeSatisfaccionSegunFelicidad (nivelDeFelicidad persona) persona
+
+-- coeficienteDeSatisfaccionSegunFelicidad :: Felicidad -> Persona -> Number
+-- coeficienteDeSatisfaccionSegunFelicidad Alta persona =  felicidonios persona * edad persona
+-- coeficienteDeSatisfaccionSegunFelicidad Media persona = felicidonios persona * sueños persona
+-- coeficienteDeSatisfaccionSegunFelicidad Baja persona = felicidonios persona `div` 2
+
+-- coeficienteDeSatisfaccion'' :: Persona -> Number
+-- coeficienteDeSatisfaccion persona = case (nivelDeFelicidad persona) of
+--     Baja -> felicidonios persona `div` 2
+--     Media -> felicidonios persona * sueños persona
+--     Alta -> felicidonios persona * edad persona
+
+-- Anoten el nombre:
+
+-- Juan
+-- Tomi
+-- Ciro
+-- Valentina
+-- Juani
+-- Maxi
--- src/Spec.hs
+++ src/Spec.hs
@@ -4,11 +4,27 @@ import Library
 import Test.Hspec
 import Control.Exception (evaluate)
 
+unaPersona :: Persona
+unaPersona = Persona { felicidonios = 10, sueños = 1, edad = 20}
+
 correrTests :: IO ()
 correrTests = hspec $ do
    describe "TP Integrador" $ do
-     it "2 + 2 es 4" $ do
-       2 + 2 `shouldBe` 4
+     describe "coeficiente de satisfaccion" $ do
+       it "si es poco feliz (tiene 50 o menos felicidonios) es los felicidonios divido 2" $ do
+         coeficienteDeSatisfaccion (unaPersona { felicidonios = 10 }) `shouldBe` 5
+         coeficienteDeSatisfaccion (unaPersona { felicidonios = 50}) `shouldBe` 25
+       it "si es moderadamente feliz (tiene mas de 50 y menor o igual a 100 felicidonios) es la cantidad de sueños por los felicidonios" $ do
+         coeficienteDeSatisfaccion (unaPersona {felicidonios = 60, sueños= 2}) `shouldBe` 120
+       it "si es muy feliz (más de 100 felicidonios) es el producto entre los felicidonios y la edad" $ do
+         coeficienteDeSatisfaccion (unaPersona {felicidonios = 110, edad = 20}) `shouldBe` 2200
+     describe "Grado de ambicion" $ do
+       it "si es muy feliz es los felicidonios multiplicado por la cantidad de sueños" $ do
+         gradoDeAmbicion (unaPersona {felicidonios = 150, sueños = 4}) `shouldBe` 600
+       it "si es moderadamente feliz es el producto entre la edad y la cantidad de sueños" $ do
+         gradoDeAmbicion (unaPersona {edad = 20, sueños = 3, felicidonios = 70}) `shouldBe` 60
+       it "si es poco feliz es el doble de la cantidad de sueños" $ do
+         gradoDeAmbicion (unaPersona{felicidonios=10, sueños=3}) `shouldBe` 6
 
 escribime :: Expectation
 escribime = implementame
